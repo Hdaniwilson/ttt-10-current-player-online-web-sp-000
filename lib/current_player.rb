d12476344135fafbcc75ee6ac56368c66ba7c5a7
Heather Wilson <hdaniwilson@gmail.com>
2021-04-10 15:46:08 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,10 @@
 def turn_count(board)
-  turns= 0
-  board.each do |player|
-    if player == "X" || player == "O"
-      turns += 1
+  count= 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      count += 1
     end
   end
+  count
   end
+  
